77d64d7ba0b908246c1d8c871fe294d425e37686
Lorenzo J Gonzalez <soporte@spahost.es>
2013-10-13 17:45:38 +0200

    / ‘lb-admin/template/headers.php’

--- lb-admin/template/headers.php
+++ lb-admin/template/headers.php
@@ -16,16 +16,14 @@ echo '<!DOCTYPE html>
   <meta charset="utf-8">
   <title>Libre Social Blog - ',ucwords($pag_pers),'</title>
   <meta name="description" content="Libre Open Source Management System for News and blogs">
-  <meta name="author" content="Lorenzo J">
   <meta name="author" content="',$lb_author,'">
   <link rel="alternate" type="application/rss+xml" title="',$lb_title,' (RSS 2.0)"" href="http://',$_SERVER["HTTP_HOST"],'/feed" />
   <link rel="shortcut icon" href="http://',$_SERVER["HTTP_HOST"],'/favicon.png" type="image/png" />
-  <link rel="stylesheet" media="screen" href="/lb-template/',$lb_template,'/css/bootstrap.min.css" />
-  <link rel="stylesheet" media="screen" href="/lb-template/',$lb_template,'/css/bootstrap-responsive.min.css" />
-  <link rel="stylesheet" media="screen" href="/lb-template/',$lb_template,'/css/datepicker.css" />
-  <link rel="stylesheet" media="screen" href="/lb-template/',$lb_template,'/css/prettify.css" />
-  <link rel="stylesheet" media="screen" href="/lb-template/',$lb_template,'/css/wysihtml5.css" />
-  <link rel="stylesheet" media="screen" href="/lb-template/',$lb_template,'/style.css" />
+  <link rel="stylesheet" media="screen" href="/lb-template/default/css/bootstrap.min.css" />
+  <link rel="stylesheet" media="screen" href="/lb-template/default/css/datepicker.css" />
+  <link rel="stylesheet" media="screen" href="/lb-template/default/css/prettify.css" />
+  <link rel="stylesheet" media="screen" href="/lb-template/default/css/wysihtml5.css" />
+  <link rel="stylesheet" media="screen" href="/lb-template/default/style.css" />
 </head>
 <!-- Termina la cabecera -->
 <body onload="prettyPrint()" bgcolor="white">';
