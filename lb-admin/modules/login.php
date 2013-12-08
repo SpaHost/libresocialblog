16b7018876dc0b0636c0e136f9ade67dec0d9e77
Lorenzo J. Gonzalez <soporte@spahost.es>
2013-12-09 00:29:28 +0100

    / ‘lb-admin/modules/login.php’

--- lb-admin/modules/login.php
+++ lb-admin/modules/login.php
@@ -12,40 +12,151 @@ if(!defined('lsb_')) die('No esta permitido acceder a esta pagina.');
 // Navbar
 echo '
   <style>
-body { overflow:hidden; }
-body { padding-bottom: 40px;color: #5a5a5a; }
-body { margin-top: 50px; padding-bottom: 40px; min-width:780px; font-size: 13px; line-height: 18px; font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; background: url(\'/vt-template/',$vt_template,'/img/back.jpg\'); }
-.login .container { position: relative; margin: 170px auto; }
-.login-wall { position: absolute; top: 0; left: 0; right: 0; bottom: 0; min-height: 100%; }
-.boton-log { margin: -30px 0px 0px -30px; }
-.login  h1, .login  .lead { margin: 0; line-height: 1.25; color: #fff; text-align: center; text-shadow: 0 1px 4px rgba(0,0,0,.4); }
-.leadi { max-width: 300px; padding: 0 29px 29px; margin: 0 auto 20px; }
+body {
+    overflow:hidden; padding-bottom: 40px; color: #5a5a5a;
+
+
+    font-size: 13px; line-height: 18px; font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;  }
+    .navbar .navbar-inner { border: 0; -webkit-box-shadow: 0 2px 10px rgba(0,0,0,.25); -moz-box-shadow: 0 2px 10px rgba(0,0,0,.25); box-shadow: 0 2px 10px rgba(0,0,0,.25); }
+    .navbar .nav > li > a { padding: 15px 20px; }
+    .navbar .btn-navbar { margin-top: 10px; }
+
+    /* CUSTOMIZE THE CAROUSEL
+    -------------------------------------------------- */
+    /* Carousel base class */
+
+    .login .container {
+      position: relative;
+      margin-top: 130px
+    }
+ 
+    .login-wall {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width: 100%;
+    }
+
+.boton-log {
+  margin: -30px 0px 0px -30px;
+}
+.login  h1,
+.login  .lead {
+      margin: 0;
+      line-height: 1.25;
+      color: #fff;
+      text-align: center;
+      text-shadow: 0 1px 4px rgba(0,0,0,.4);
+    }
+
+.leadi {
+        max-width: 300px;
+        padding: 0 29px 29px;
+        margin: 0 auto 20px;
+}
+
 .fofoter { position: fixed; font-size: 0.75em; color: #fff; text-shadow: 0 1px 1px rgba(0,0,0,.4); text-decoration: none; bottom: 5px; left: 25px; }
 .fofoter a:hover { text-decoration: none; }
-.leadi a, .leadi a:hover{ margin: 0; font-size: 0.85em; color: #000; text-shadow: 0 1px 1px rgba(0,0,0,.4); text-decoration: none; margin: 0 auto; }
-.form-signin { max-width: 300px; padding: 19px 29px 29px; margin: 0 auto 20px; background-color: #fff; border: 1px solid #e5e5e5; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; -webkit-box-shadow: 0 1px 7px rgba(0,0,0,.5); -moz-box-shadow: 0 1px 7px rgba(0,0,0,.5); box-shadow: 0 1px 7px rgba(0,0,0,.5); }
-.form-signin .form-signin-heading, .form-signin .checkbox { margin-bottom: 10px; }
-.form-signin input[type="text"], .form-signin input[type="password"] { font-size: 16px; height: auto; margin-bottom: 15px; padding: 7px 9px; }
-.alert { margin-top: -50px; width:70%; margin-left: 17%; -webkit-box-shadow: 0 1px 7px rgba(0,0,0,.5); -moz-box-shadow: 0 1px 7px rgba(0,0,0,.5); box-shadow: 0 1px 7px rgba(0,0,0,.5); }
 
-@media (max-width: 979px) {
-  .container.navbar-wrapper { margin-bottom: 0; width: auto; }
-  .navbar-inner { border-radius: 0; margin: -20px 0; }
-  .login .container { position: relative; margin-top: 145px }
-  .login-wall { width: auto; height: 400px; }
+
+.leadi a,
+.leadi a:hover{
+      margin: 0;
+      font-size: 0.85em;
+      color: #fff;
+      text-shadow: 0 1px 1px rgba(0,0,0,.4);
+      text-decoration: none;
+      margin: 0 auto;
+
+    }
+
+
+     .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 7px rgba(0,0,0,.5);
+           -moz-box-shadow: 0 1px 7px rgba(0,0,0,.5);
+                box-shadow: 0 1px 7px rgba(0,0,0,.5);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
+
+.alert {
+	margin-top: -50px;
+	width:70%;
+	margin-left: 10%;
+	        -webkit-box-shadow: 0 1px 7px rgba(0,0,0,.5);
+           -moz-box-shadow: 0 1px 7px rgba(0,0,0,.5);
+                box-shadow: 0 1px 7px rgba(0,0,0,.5);
 }
 
-@media (max-width: 767px) {
-  .navbar-inner { margin: -20px; }
-  .text-center img { width: 40% }
-  .login { margin-top: -80px; margin-left: -55%; }
-  .login .container { }
-  .login-wall { height: 300px; }
-  .login .container{ width: 65%; padding: 0 70px; margin-top: 80px; }
-  .login .container h1 { font-size: 30px; }
-  .form-signin { margin-top: 10px; }
-  .leadi a { font-color:#000; }
+
+
+    /* RESPONSIVE CSS
+    -------------------------------------------------- */
+
+    @media (max-width: 979px) {
+
+      .container.navbar-wrapper { margin-bottom: 0; width: auto; }
+      .navbar-inner { border-radius: 0; margin: -20px 0; }
+      .login .container { position: relative; margin-top: 175px }
+      .login-wall { width: auto; height: 400px; min-width: 100%;}
+
+    }
+
+
+    @media (max-width: 767px) {
+
+      .navbar-inner {
+        margin: -20px;
+      }
+
+      .login {
+        margin-left: -20px;
+        margin-right: -20px;
+      }
+      .login .container {
+
+      }
+
+      .login-wall {
+        width: 100%;
+        height: 300px;
+      }
+      .login .container{
+        width: 65%;
+        padding: 0 70px;
+        margin-top: 80px;
+      }
+      .login .container h1 {
+        font-size: 30px;
+      }
+.form-signin { margin-top: 10px;}
+.leadi a {
+        font-color:#000;
 }
+
+    }
+
+
+
     </style>
 
 
