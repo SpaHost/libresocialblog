f95c5e260263e1d73792df145cc39663e6cb5e10
Lorenzo J. Gonzalez <soporte@spahost.es>
2013-12-09 07:06:41 +0100

    / ‘lb-admin/modules/home.php’

--- lb-admin/modules/home.php
+++ lb-admin/modules/home.php
@@ -31,17 +31,17 @@ if(!defined('lsb_')) die('No esta permitido acceder a esta pagina.');
 
 
   <!--[if lt IE 9]>
-    <script src="/js/html5shiv.js"></script>
-    <script src="/js/respond.min.js"></script>
+    <script src="/lb-template/default/js/html5shiv.js"></script>
+    <script src="/lb-template/default/js/respond.min.js"></script>
   <![endif]-->
 
-<script src="/js/wysihtml5-0.3.0.js"></script>
-<script src="/js/jquery.js"></script>
-<script src="/js/prettify.js"></script>
-<script src="/js/bootstrap.min.js"></script>
-<script src="/js/bootstrap-datepicker.js"></script>
-<script src="/js/wysihtml5.js"></script>
-<script src="/js/wysihtml5-es.js"></script>
+<script src="/lb-template/default/js/wysihtml5-0.3.0.js"></script>
+<script src="/lb-template/default/js/jquery.js"></script>
+<script src="/lb-template/default/js/prettify.js"></script>
+<script src="/lb-template/default/js/bootstrap.min.js"></script>
+<script src="/lb-template/default/js/bootstrap-datepicker.js"></script>
+<script src="/lb-template/default/js/wysihtml5.js"></script>
+<script src="/lb-template/default/js/wysihtml5-es.js"></script>
 
 <script>
   $('.textarea-post').wysihtml5({
